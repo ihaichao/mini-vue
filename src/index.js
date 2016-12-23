a81d2ffff75430a92035ca722491ab357bb0d9a7
zhangzongzheng <zhangzongzheng@bilibili.com>
2016-12-23 18:55:10 +0800

    update

--- src/index.js
+++ src/index.js
@@ -4,39 +4,81 @@
 
 class Vue {
   constructor (options) {
+    this.el = options.el
+    this.node = document.querySelector(this.el)
     this.data = options.data
-    observer(this.data, this._update)
+    // observer(this.data, function () {this.scan(this.node, this.data)})
+    // watcher(this.scan(this.node, this.data))
+    // watcher(this, this._update)
+    this.scan(this.node, this.data)
   }
 
   _update () {
     console.log('data updated')
   }
+
+  scan (node, data) {
+    for (let i = 0; i < node.children.length; i++) {
+      this.parseText(node.children[i], data)
+    }
+  }
+
+  parseText (node, data) {
+    let prop = node.getAttribute('v-text')
+    if (prop.indexOf('.') === -1) {
+      node.innerText = data[prop]
+      observer(data, function (old, now) {
+        node.innerText = now
+      })
+    }
+  }
 }
 
-function observer (data, callback) {
+class Dep {
+  constructor () {
+    this.subs = []
+  }
+
+  add (callback) {
+    this.subs.push(callback)
+  }
+
+  notify () {
+    this.subs.forEach((sub) => {
+      sub()
+    })
+  }
+}
+
+function observer (data, cb) {
   Object.keys(data).forEach((key) => {
-    dataReactive(data, key, data[key], callback)
+    defineReactive(data, key, data[key], cb)
   })
 }
 
-function dataReactive (data, key, val, callback) {
+// function watcher (callback) {
+//   Dep.target = callback
+// }
+
+function defineReactive (data, key, val, cb) {
+  let dep = new Dep()
+  
   Object.defineProperty(data, key, {
     enumerable: true,
     configurable: true,
     get: () => {
+      // dep.add(Dep.target)
       return val
       // return data[key] // Maximum call stack size exceeded(…) http://stackoverflow.com/questions/23938419/using-object-defineproperty-with-rangeerror
     },
     set: (newVal) => {
-      val = newVal
-      typeof callback === 'function' && callback()
+      if (val === newVal) {
+        return
+      } else {
+        val = newVal
+        // dep.notify()
+        cb(val, newVal)
+      }
     }
   })
 }
-
-const app = new Vue({
-  el: '#app',
-  data: {
-    msg: 'hello world'
-  }
-})
